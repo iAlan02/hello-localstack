b0a9f706f0c7144430aab30f05040954495cd97f
Ruiseco <acisneros@expediagroup.com>
2023-05-30 21:30:55 -0600

    commented s3_force_path_style property on aws provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region                      = "eu-west-1"
   access_key                  = "fake_access_key"
   secret_key                  = "fake_secret_key"
-  s3_force_path_style         = true
+  # s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
